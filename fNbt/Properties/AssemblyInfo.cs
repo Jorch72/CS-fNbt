0602d4ddef4bd9b3149630930707ede5aeeaa2d4
NiclasOlofsson <nicke.olofsson@gmail.com>
2016-10-02 15:14:30 +0200

    Release 0.6.4

--- fNbt/Properties/AssemblyInfo.cs
+++ fNbt/Properties/AssemblyInfo.cs
@@ -36,8 +36,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.6.3.0")]
-[assembly: AssemblyFileVersion("0.6.3.0")]
+[assembly: AssemblyVersion("0.6.4.0")]
+[assembly: AssemblyFileVersion("0.6.4.0")]
 
 // Potentially speed up resource probes
 
